5eee3668492123be95ea94db139614fe82d13395
waku <wakunguma13@gmail.com>
2025-06-02 08:36:42 +0200

    feat: add io and toml error variants

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,13 @@
+use std::io;
+
 use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T,Error>;
 
 #[derive(Debug,Error)]
 pub enum Error{
-
+	#[error(transparent)]
+	Io(#[from] io::Error),
+	#[error(transparent)]
+	TomlDeserialize(#[from] toml::de::Error)
 }
